7df5ff18a1e8d4a5327e55e3c99ab0369ac1dd97
FurryCat <furrycat.web@gmail.com>
2019-12-17 23:25:03 +0300

    Lesson 6. First question

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from "./ActiveQuiz.module.css";
+import AnswersList from "./AnswersList/AnswersList";
 
 const ActiveQuiz = props => (
     <div className={classes.ActiveQuiz}>
@@ -13,13 +14,8 @@ const ActiveQuiz = props => (
             <small>4 из 12</small>
         </p>
 
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-        </ul>
+        <AnswersList 
+            answers={props.answers} />
     </div>
 );
 
---
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from "./AnswersList.module.css";
+import AnswerItem from "./AnswerItem/AnswerItem";
+
+const AnswersList = props => (
+    <ul className={classes.AnswersList}>
+        { props.answers.map((answer, index) => {
+            return (
+                <AnswerItem 
+                    answer={answer} 
+                    key={index} />
+            );
+        }) }
+    </ul>
+);
+
+export default AnswersList;
