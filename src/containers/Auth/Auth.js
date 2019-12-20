86c1d0a2c62c0bae07b6f1544f464eb479415a8a
FurryCat <furrycat.web@gmail.com>
2019-12-20 12:28:06 +0300

    Forms 4. Validate inputs

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -4,7 +4,40 @@ import classes from "./Auth.module.css";
 import Button from "../../components/UI/Button/Button"
 import Input from "../../components/UI/Input/Input"
 
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 export default class Auth extends Component {
+    state = {
+        formControls: {
+            email: {
+                value: '',
+                type: 'email',
+                label: 'Email',
+                errorMessage: 'Введите корректный email',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    email: true,
+                }
+            },
+            password: {
+                value: '',
+                type: 'password',
+                label: 'Password',
+                errorMessage: 'Введите корректный пароль',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    minLength: 6
+                }
+            }
+        }
+    }
+
     loginHandler = () => {
 
     }
@@ -17,6 +50,62 @@ export default class Auth extends Component {
         event.preventDefault();
     }
 
+
+
+    validateControl(value, validation) {
+        if (!validation) return true;
+
+        let isValid = true;
+
+        if (validation.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (validation.email) {
+            isValid = validateEmail(value) && isValid;
+        }
+
+        if (validation.minLength) {
+            isValid = value.length >= validation.minLength && isValid;
+        }
+
+        return isValid;
+    }
+
+    onChangeHandler = (event, controlName) => {
+        const formControls = { ...this.state.formControls };
+        const control = { ...formControls[controlName] };
+
+        control.value = event.target.value;
+        control.touched = true;
+        control.valid = this.validateControl(control.value, control.validation);
+
+        formControls[controlName] = control;
+
+        this.setState({
+            formControls
+        });
+    }
+
+    renderInputs() {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName];
+            return (
+                <Input key={controlName + index}
+                    onChange={event => this.onChangeHandler(event, controlName)}
+                    shouldValidate={!!control.validation}
+                    {...control}
+                    // type={control.type}
+                    // valid={control.valid}
+                    // value={control.value}
+                    // touched={control.touched}
+                    // label={control.label}
+                    // errorMessage={control.errorMessage}
+                />
+            )
+        });
+    }
+
     render() {
         return (
             <div className={classes.Auth}>
@@ -25,15 +114,8 @@ export default class Auth extends Component {
 
                     <form onSubmit={this.submitHandler}
                         className={classes.AuthForm}>
-                        <Input
-                            errorMessage={'TEST'}
-                            label="Email"
-                        />
                         
-                        <Input
-                          
-                            label="Пароль"
-                        />
+                        { this.renderInputs() }
 
                         <Button
                             onClick={this.loginHandler}
